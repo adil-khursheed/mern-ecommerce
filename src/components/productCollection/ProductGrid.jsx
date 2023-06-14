43bd8bed504d3d9fd6da12e000a824d77856be78
adil-khursheed <adilkhursheed60@gmail.com>
2023-06-14 12:23:06 +0530

    improved filter logic

---
+++ src/components/productCollection/ProductGrid.jsx
@@ -0,0 +1,53 @@
+import { StarIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+
+const ProductGrid = ({ products }) => {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-4">
+          {products.map((product) => (
+            <Link to={"/product-detail"} key={product.id}>
+              <div className="group relative border-solid border-2 border-gray-200 rounded-md p-2">
+                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
+                  <img
+                    src={product.thumbnail}
+                    alt={product.title}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <div>
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {product.title}
+                      </div>
+                    </h3>
+                    <p className="mt-1 text-sm text-gray-500 flex items-center gap-1">
+                      <StarIcon className="w-4 h-4" />
+                      <span className="mt-[2px]">{product.rating}</span>
+                    </p>
+                  </div>
+                  <div className="flex flex-col items-end">
+                    <p className="text-sm font-medium text-gray-900">
+                      $
+                      {Math.round(
+                        product.price * (1 - product.discountPercentage / 100)
+                      )}
+                    </p>
+                    <p className="text-sm font-medium text-gray-400 line-through">
+                      ${product.price}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductGrid;
--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -6,11 +6,25 @@ export function fetchAllProducts() {
   });
 }
 
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter, sort) {
+  // filter = {"category":["smartphone","laptops"]}
+  // sort = {_sort:"price",_order="desc"}
+
+  // TODO : on server we will support multiple values
+
   let queryString = "";
   for (let key in filter) {
-    queryString += `${key}=${filter[key]}&`;
+    const categoryValues = filter[key];
+    if (categoryValues.length) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
   }
+
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+
   return new Promise(async (resolve) => {
     const response = await fetch(
       "http://localhost:8080/products?" + queryString
